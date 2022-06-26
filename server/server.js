92ab1f2e6024c229636640eb0b31979057009e10
GregoryGarber <ggarber@umass.edu>
2022-06-26 15:20:05 -0500

    fix6

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
-const express = require('express');
-const cors = require('cors');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
+app.use(logger("dev"));
 app.use(express.json());
-app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-const port = process.env.PORT || 8082;
+
 
 app.get('/test', (req, res) => res.send('Test server!'));
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.send(err);
+  });
 
 
-
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
 module.exports = app;
